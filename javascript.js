bc24cedd5b781fd86904af6da56837b9b1b23868
kittycoder678 <felicitywright2410@gmail.com>
2025-06-11 14:13:36 +1200

    Got menu bar to change color

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
 function openMenu() {
     menuBar.style.display = 'initial';
+    menuTest.style.display = 'initial';
+    menuTitle.style.display = 'initial';
     menuBox.style.display = 'none';
+    menuTitles.style.display = 'none';
 }
 function reset() {
     menuBox.style.display = 'initial';
     menuBar.style.display = 'none';
+    menuTitles.style.display = 'initial';
+    menuTest.style.display = 'none';
+    menuTitle.style.display = 'none';
+}
+function hidden(){
+menuTitle.style.display = 'none';    
 }
 menuBox.onmouseover = openMenu;
 menuBar.onmouseleave = reset;
+menuTitle.menuBox= hidden;
